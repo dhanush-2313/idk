bdf3831f85302d3a2fd7fc95c58e43d1570bee62
dhanush-2313 <dhanusu006@gmail.com>
2024-10-01 09:31:01 +0530

    captcha

--- 5-rate-limiter/backend/src/index.ts
+++ 5-rate-limiter/backend/src/index.ts
@@ -1,34 +1,21 @@
 import express from "express";
-import rateLimit from "express-rate-limit";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY || "default_secret_key";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-// Rate limiter configuration
-const otpLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 3, // Limit each IP to 3 OTP requests per windowMs
-  message: "Too many requests, please try again after 5 minutes",
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-
-const passwordResetLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 password reset requests per windowMs
-  message:
-    "Too many password reset attempts, please try again after 15 minutes",
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+app.use(cors());
 
 // Store OTPs in a simple in-memory object
 const otpStore: Record<string, string> = {};
 
-// Endpoint to generate and log OTP with rate limiting
-app.post("/generate-otp", otpLimiter, (req, res) => {
+// Endpoint to generate and log OTP
+app.post("/generate-otp", (req, res) => {
   console.log(req.body);
   const email = req.body.email;
   if (!email) {
@@ -41,9 +28,25 @@ app.post("/generate-otp", otpLimiter, (req, res) => {
   res.status(200).json({ message: "OTP generated and logged" });
 });
 
-// Endpoint to reset password with rate limiting
-app.post("/reset-password", passwordResetLimiter, (req, res) => {
-  const { email, otp, newPassword } = req.body;
+// Endpoint to reset password
+app.post("/reset-password", async (req, res) => {
+  const { email, otp, newPassword, token } = req.body;
+  console.log(token);
+
+  let formData = new FormData();
+  formData.append("secret", SECRET_KEY);
+  formData.append("response", token);
+
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+  const result = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+  const challengeSucceeded = (await result.json()).success;
+
+  if (!challengeSucceeded) {
+    return res.status(403).json({ message: "Invalid reCAPTCHA token" });
+  }
 
   if (!email || !otp || !newPassword) {
     return res
